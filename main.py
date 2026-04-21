d6387ef206578109eb9f6cc334dc340974c24db3
Ryan Guinchard <ryanguinchard9@hotmail.com>
2026-04-21 18:12:18 -0230

    basic working model with parameter insertion

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-def main():
-    print("Hello from aiagent!")
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import argparse
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+#parsing user input
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 
-if __name__ == "__main__":
-    main()
+
+# Make the API call
+response = client.models.generate_content(
+    model="gemini-2.5-flash", 
+    contents=messages
+    )
+
+
+
+# Showing the results of the response and usage metadata if verbose flag is set
+if args.verbose:
+    if response.usage_metadata is None:
+        raise RuntimeError("Usage metadata is not available")
+    else:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+print(response.text)
